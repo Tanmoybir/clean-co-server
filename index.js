07f6312e50a7d32ff7afa0ab206c0216c5d889d6
Tanmoy Bir <tanmoy7468@gmail.com>
2024-03-07 20:25:07 +0600

    filtering by cayegory and sorting and pagination

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 
 // middleWare
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}))
 app.use(express.json())
 app.use(cookieParser())
 
-const secret = 'veryveryscretamikaokadivona'
+const secure = 'veryveryasfefgrg5tg'
 
 // user:clean-co 
 // pass:u6gEMp6PVGpoJsI0
@@ -35,29 +38,22 @@ async function run() {
         const serviceCollection = client.db('clean-co').collection('services')
         const bookingCollection = client.db('clean-co').collection('bookings')
 
-        const gateman = (req,res,next) => {
-            const {token} = req.cookies
+        const gateman = (req, res, next) => {
+            const { token } = req.cookies
             // console.log(token);
-
-            if(!token) {
-                return res.status(401).send({message:"you are not authorize"})
+            if (!token) {
+                return res.status(401).send({ message: 'You are not Authorize' })
             }
-
-            jwt.verify(token, secret, function(err, decoded) {
-                if(err){
-                    return res.status(401).send({message:"you are not authorize"})
+            // verify a token symmetric
+            jwt.verify(token, secure, function (err, decoded) {
+                if (err) {
+                    return res.status(401).send({ message: 'You are not Authorize' })
                 }
                 req.user = decoded
                 // console.log(decoded);
                 next()
-              });
+            });
         }
-
-        app.get('/api/v1/services',gateman, async (req, res) => {
-            const cursor = serviceCollection.find()
-            const result = await cursor.toArray()
-            res.send(result)
-        })
 
         app.get('/api/v1/services/:id', async (req, res) => {
             const id = req.params.id
@@ -65,7 +61,46 @@ async function run() {
             const result = await serviceCollection.findOne(query)
             res.send(result)
         })
-       
+
+        // Filtering API Format 
+        //http:localhost:5000/api/v1/services(situation-1)
+        //http:localhost:5000/api/v1/services?category=Specialized Cleaners(situation-2)
+
+        // Sorting API Format
+        //http:localhost:5000/api/v1/services?sortFiled=price && sortOrder=asc(situation-3)
+        //http:localhost:5000/api/v1/services?sortFiled=price && sortOrder=desc(situation-4)
+        // Pagination API Format
+        app.get('/api/v1/services',gateman, async (req, res) => {
+
+            let queryObj = {};
+            let sortObj = {};
+            // filter
+            const category = req.query.category;
+            // sort
+            const sortField = req.query.sortField;
+            const sortOrder = req.query.sortOrder;
+            // Pagination
+            const page = Number(req.query.page);
+            const limit = Number(req.query.limit);
+            const skip = (page - 1) * limit
+            if (category) {
+                queryObj.category = category
+            }
+
+            if (sortField && sortOrder) {
+                sortObj[sortField] = sortOrder
+            }
+
+            const cursor = serviceCollection.find(queryObj).skip(skip).limit(limit).sort(sortObj)
+            const result = await cursor.toArray()
+            // CountData
+            const total = await serviceCollection.countDocuments()
+            res.send({
+                total,
+                result
+            })
+        })
+
         app.post('/api/v1/user/create-booking', async (req, res) => {
             const booking = req.body
             // console.log(booking);
@@ -75,20 +110,21 @@ async function run() {
 
         // user specific booking
 
-        app.get('/api/v1/user/booking',gateman, async (req, res) => {
+        app.get('/api/v1/user/booking', gateman, async (req, res) => {
             const queryEmail = req.query.email
             const tokenEmail = req.user.email
-            // console.log(queryEmail);
-            if(queryEmail !== tokenEmail) {
-                return res.status(403).send({message: 'Forbidden Access'})
+            // console.log(tokenEmail);
+            if (queryEmail !== tokenEmail) {
+                return res.status(403).send({ message: 'Forbidden access' })
             }
             let query = {}
-            if(queryEmail) {
+            if (queryEmail) {
                 query.email = queryEmail
             }
             const result = await bookingCollection.find(query).toArray()
-                res.send(result)         
+            res.send(result)
         })
+
 
         app.delete('/api/v1/user/cancel-booking/:id', async (req, res) => {
             const id = req.params.id
@@ -97,18 +133,20 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/api/v1/auth/access-token', async (req, res) => {
 
-        app.post('/api/v1/auth/access-token', (req, res) => {
-            // creating token and send to client
             const user = req.body
-            const token = jwt.sign(user, secret)
+            const token = jwt.sign(user, secure)
             // console.log(token);
             res.cookie('token', token, {
                 httpOnly: true,
                 secure: true,
-                sameSite: false
+                sameSite: 'none'
             }).send({ success: true })
+
+
         })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
